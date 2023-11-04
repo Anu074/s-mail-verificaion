cfaa3b330c661c5ee2c884143158ac50c0fed2f0
Anu074 <psarojanurag@gmail.com>
2023-11-04 13:19:09 +0530

    updated scrip.js

---
+++ hello-init.js
@@ -0,0 +1,6 @@
+// hello-init.js
+
+// Initialize hello.js with your app credentials (OAuth client ID) for Google
+hello.init({
+    google: '420315943053-advemgmc4qq0s801bld4de8dfenaof1m.apps.googleusercontent.com'
+});
---
+++ script.js
@@ -0,0 +1,39 @@
+// Listen for the button click event
+document.getElementById('verify-button').addEventListener('click', function() {
+    // Trigger the Google authentication flow using hello('google').login
+    hello('google').login({ scope: 'email' }).then(function(authResponse) {
+        // Handle the authentication response
+        handleAuthenticationResponse(authResponse);
+    }).catch(function(error) {
+        // Handle authentication errors
+        console.error('Authentication failed', error);
+    });
+});
+
+
+// Function to handle the authentication response
+function handleAuthenticationResponse(authResponse) {
+    if (authResponse && authResponse.authResponse) {
+        // The user is authenticated
+
+        // Access the user's email (or other data) from the authentication response
+        var email = authResponse.authResponse.email;
+
+        // Check the user's email for validation, e.g., verifying the domain
+        if (email && email.endsWith('@ds.study.iitm.ac.in')) {
+            // Email is valid
+
+            // You can proceed to perform actions like assigning a "verify" role in Discord
+            // Add your logic here
+
+            // Example: Display a success message
+            console.log('Authentication successful');
+        } else {
+            // Email is not valid
+            console.log('Invalid email address');
+        }
+    } else {
+        // Authentication failed or was canceled
+        console.log('Authentication failed or was canceled');
+    }
+}
